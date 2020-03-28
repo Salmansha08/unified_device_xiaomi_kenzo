d059860a425d1d468bb6a4437f577868bf8baf0e
Sagarrokade006 <sagarrokade006@gmail.com>
2020-03-28 01:41:42 +0000

    Exclude QCOM Sepolicy & Nuke lineage power from manifest

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -229,6 +229,7 @@ TARGET_LD_SHIM_LIBS := \
     /system/lib64/lib-imsvideocodec.so|libshim_ims.so
 
 # SELinux
+TARGET_EXCLUDE_QCOM_SEPOLICY := true
 BOARD_SEPOLICY_VERS := 29.0
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
