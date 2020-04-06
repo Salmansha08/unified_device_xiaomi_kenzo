c8545ed1a78d209e5764aed64856dc66d00f9840
Sagarrokade006 <sagarrokade006@gmail.com>
2020-04-06 11:07:30 +0000

    Finally Booted
    - Re-enable sound trigger
    - Re-arrange sensorservice and schedulerservice on compatibility_matrix
    - Don't build libqdmetadata
    - Build libmm-omxcore
    - Pin framework and service

--- device.mk
+++ device.mk
@@ -244,8 +244,6 @@ PRODUCT_PACKAGES += \
     memtrack.msm8952 \
     libdisplayconfig \
     liboverlay \
-    libqdMetaData \
-    libqdMetaData.system \
     libgenlock \
     vendor.display.config@1.0 \
     vendor.display.config@1.0_vendor
@@ -371,6 +369,7 @@ PRODUCT_PACKAGES += \
 
 # OMX
 PRODUCT_PACKAGES += \
+    libmm-omxcore \
     libc2dcolorconvert \
     libextmedia_jni \
     libOmxAacEnc \
